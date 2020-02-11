6cedd87cc3fe5c6880d6610edd743d9b65beb874
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-02-11 14:08:10 -0500

    Updates the success message in 'success.html'; creates a route to handle any POST requests to '/submit'

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ app = Flask(__name__)
 def index():
   return render_template('index.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+  if request.method == 'POST':
+    user = request.form['user']
+    typeOfRequest = request.form['typeOfRequest']
+    prayerRequest = request.form['prayerRequest']
+    print(user, typeOfRequest, prayerRequest)
+    return render_template('success.html')
+
 
 if __name__ == '__main__':
   app.debug = True
