d2ce7686cf7150896a960a8c89d9f2915b08c968
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-06 16:06:35 -0500

    Updates SMTP configurations for elktondotchurch@gmail.com, admin@elkton.church, and ticothep@gmail.com; updates styles and content of <form> element

--- send_mail.py
+++ send_mail.py
@@ -4,26 +4,29 @@ from email.mime.text import MIMEText
 from dotenv import load_dotenv
 load_dotenv()
 
-MAILTRAP_USERNAME = os.environ.get('MAILTRAP_USERNAME')
-MAILTRAP_PASSWORD = os.environ.get('MAILTRAP_PASSWORD')
+ELKTON_DOT_CHURCH_GMAIL_USERNAME = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_USERNAME')
+ELKTON_DOT_CHURCH_GMAIL_PASSWORD = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_PASSWORD')
 TICO_GMAIL_EMAIL = os.environ.get('TICO_GMAIL_EMAIL')
-ELKTONMC_LIVE_EMAIL = os.environ.get('ELKTONMC_LIVE_EMAIL')
 
 def send_mail(user, typeOfRequest, prayerRequest):
-    port = 2525
-    smtp_server = 'smtp.mailtrap.io'
-    login = MAILTRAP_USERNAME
-    password = MAILTRAP_PASSWORD
-    message = f"<h3>New Prayer Request Submission</h3><ul><li>User: {user}</li><li>Type of Prayer Request: {typeOfRequest}</li><li>Prayer Request: {prayerRequest}</li></ul>"
+    port = 587
+    smtp_server = 'smtp.gmail.com'
+    login = ELKTON_DOT_CHURCH_GMAIL_USERNAME
+    password = ELKTON_DOT_CHURCH_GMAIL_PASSWORD
+    message = f"<p>Dear Deacon/Deaconess,\n\nPlease remember to keep <strong>{user}</strong> in your prayers today.</p><p>{user} has specifically asked for intercessory prayer concerning <strong>{typeOfRequest}</strong> today.</p>\n\n<p>Here are some more details concerning {user}'s prayer request:</p>\n<ul><li>'{prayerRequest}'</li></ul>\n\n\n<ul><li>User: {user}</li><li>Type of Prayer Request: {typeOfRequest}</li><li>Prayer Request: {prayerRequest}</li></ul>"
     
-    sender_email = ELKTONMC_LIVE_EMAIL
-    receiver_email = TICO_GMAIL_EMAIL
+    sender = ELKTON_DOT_CHURCH_GMAIL_USERNAME
+    recipients = TICO_GMAIL_EMAIL
     msg = MIMEText(message, 'html')
-    msg['Subject'] = 'EMC Prayer Request'
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
+    msg['Subject'] = 'New EMC Prayer Request'
+    msg['From'] = sender
+    msg['To'] = recipients
     
     # Send email
     with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        
         server.login(login, password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.sendmail(sender, recipients, msg.as_string())
