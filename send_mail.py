21a9a75195a7d899991f25bd20fb75ea5184e732
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-22 13:29:28 -0400

    Refactors 'send_mail.py' file

--- send_mail.py
+++ send_mail.py
@@ -4,11 +4,13 @@ from email.mime.text import MIMEText
 from dotenv import load_dotenv
 load_dotenv()
 
+ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS = os.environ.get('ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS')
 ELKTON_DOT_CHURCH_GMAIL_USERNAME = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_USERNAME')
 ELKTON_DOT_CHURCH_GMAIL_PASSWORD = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_PASSWORD')
+
 TICO_GMAIL_EMAIL = os.environ.get('TICO_GMAIL_EMAIL')
 TICO2_GMAIL_EMAIL = os.environ.get('TICO2_GMAIL_EMAIL')
-ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS = os.environ.get('ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS')
+
 
 def send_mail(user, typeOfRequest, prayerRequest):
     port = 587
@@ -17,8 +19,8 @@ def send_mail(user, typeOfRequest, prayerRequest):
     password = ELKTON_DOT_CHURCH_GMAIL_PASSWORD
     message = f"<div><ul><li>Name: {user}</li></ul></div><div><ul><li>Type of Prayer Request: {typeOfRequest}</li></ul></div><div><ul><li>Details of Prayer Request: {prayerRequest}</li></ul></div>"
     
-    sender = ELKTON_DOT_CHURCH_GMAIL_USERNAME
-    recipients = [TICO_GMAIL_EMAIL, TICO2_GMAIL_EMAIL, ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS]
+    sender = ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS
+    recipients = [TICO_GMAIL_EMAIL, TICO2_GMAIL_EMAIL]
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'New EMC Prayer Request'
     msg['From'] = sender
