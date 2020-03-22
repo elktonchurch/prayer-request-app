7726c02a5316d2b4d58a1af89cd0ccb9ca78b288
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-22 02:43:23 -0400

    Allows for 3+ emails to get notifications after prayer request is submitted

--- send_mail.py
+++ send_mail.py
@@ -8,16 +8,17 @@ ELKTON_DOT_CHURCH_GMAIL_USERNAME = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_USERN
 ELKTON_DOT_CHURCH_GMAIL_PASSWORD = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_PASSWORD')
 TICO_GMAIL_EMAIL = os.environ.get('TICO_GMAIL_EMAIL')
 TICO2_GMAIL_EMAIL = os.environ.get('TICO2_GMAIL_EMAIL')
+ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS = os.environ.get('ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS')
 
 def send_mail(user, typeOfRequest, prayerRequest):
     port = 587
     smtp_server = 'smtp.gmail.com'
     login = ELKTON_DOT_CHURCH_GMAIL_USERNAME
     password = ELKTON_DOT_CHURCH_GMAIL_PASSWORD
-    message = f"<ul><li>User: {user}</li><li>Type of Prayer Request: {typeOfRequest}</li><li>Prayer Request: {prayerRequest}</li></ul>"
+    message = f"<div><ul><li>Name: {user}</li></ul></div><div><ul><li>Type of Prayer Request: {typeOfRequest}</li></ul></div><div><ul><li>Details of Prayer Request: {prayerRequest}</li></ul></div>"
     
     sender = ELKTON_DOT_CHURCH_GMAIL_USERNAME
-    recipients = [TICO_GMAIL_EMAIL, TICO2_GMAIL_EMAIL]
+    recipients = [TICO_GMAIL_EMAIL, TICO2_GMAIL_EMAIL, ADMIN_ELKTON_CHURCH_EMAIL_ADDRESS]
     msg = MIMEText(message, 'html')
     msg['Subject'] = 'New EMC Prayer Request'
     msg['From'] = sender
