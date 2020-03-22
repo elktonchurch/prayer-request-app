355b1a648244d59084ecadf42b2bb68e9cdbe986
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-22 01:59:37 -0400

    Refactors message displayed on success of form submission

--- send_mail.py
+++ send_mail.py
@@ -13,7 +13,7 @@ def send_mail(user, typeOfRequest, prayerRequest):
     smtp_server = 'smtp.gmail.com'
     login = ELKTON_DOT_CHURCH_GMAIL_USERNAME
     password = ELKTON_DOT_CHURCH_GMAIL_PASSWORD
-    message = f"<p>Dear Deacon/Deaconess,\nPlease remember to keep <strong>{user}</strong> in your prayers today.</p><p>{user} has specifically asked for intercessory prayer concerning <strong>{typeOfRequest}</strong> today.</p>\n\n<p>Here are some more details concerning {user}'s prayer request:</p>\n<ul><li>'{prayerRequest}'</li></ul>\n\n\n<ul><li>User: {user}</li><li>Type of Prayer Request: {typeOfRequest}</li><li>Prayer Request: {prayerRequest}</li></ul>"
+    message = f"<ul><li>User: {user}</li><li>Type of Prayer Request: {typeOfRequest}</li><li>Prayer Request: {prayerRequest}</li></ul>"
     
     sender = ELKTON_DOT_CHURCH_GMAIL_USERNAME
     recipients = TICO_GMAIL_EMAIL
