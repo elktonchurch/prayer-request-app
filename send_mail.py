14c692c026f71d23e5e6ce0853b960dec60c54b9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-03-29 03:35:45 -0400

    Adds pastoral staff's emails, deacons' emails, and deaconesses' emails

--- send_mail.py
+++ send_mail.py
@@ -4,16 +4,30 @@ from email.mime.text import MIMEText
 from dotenv import load_dotenv
 load_dotenv()
 
-# elkton.church email addresses
+# https://elkton.church Email Addresses
 ADMIN_ELKTON_CHURCH_EMAIL = os.environ.get('ADMIN_ELKTON_CHURCH_EMAIL')
 SUPPORT_ELKTON_CHURCH_EMAIL = os.environ.get('SUPPORT_ELKTON_CHURCH_EMAIL')
 
-# elktondotchurch gmail addresses
+# elktondotchurch@gmail.com credentials
 ELKTON_DOT_CHURCH_GMAIL_USERNAME = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_USERNAME')
 ELKTON_DOT_CHURCH_GMAIL_PASSWORD = os.environ.get('ELKTON_DOT_CHURCH_GMAIL_PASSWORD')
 
+# Pastoral Team's Email Addresses
+PASTOR_REGGIE_EMAIL = os.environ.get('PASTOR_REGGIE_EMAIL')
+SUE_COURLISS_EMAIL = os.environ.get('SUE_COURLISS_EMAIL')
+
 # Deacons' Email Addresses
 TICO_GMAIL_EMAIL = os.environ.get('TICO_GMAIL_EMAIL')
+DON_FAUPEL_EMAIL = os.environ.get('DON_FAUPEL_EMAIL')
+CASEY_TURNER_EMAIL = os.environ.get('CASEY_TURNER_EMAIL')
+MIKE_VIERS_EMAIL = os.environ.get('MIKE_VIERS_EMAIL')
+
+# Deaconesses' Email Addresses
+MELISSA_HELMUTH_EMAIL = os.environ.get('MELISSA_HELMUTH_EMAIL')
+KIM_KARL_EMAIL = os.environ.get('KIM_KARL_EMAIL')
+KELLEE_ROTH_EMAIL = os.environ.get('KELLEE_ROTH_EMAIL')
+PATTY_EVANS_EMAIL = os.environ.get('PATTY_EVANS_EMAIL')
+JEN_VIERS_EMAIL = os.environ.get('JEN_VIERS_EMAIL')
 
 # Sends out multiple email notifications of a new EMC prayer request
 def send_mail(userFirstName, userLastName, typeOfRequest, prayerRequest):
@@ -27,7 +41,19 @@ def send_mail(userFirstName, userLastName, typeOfRequest, prayerRequest):
     sender = ADMIN_ELKTON_CHURCH_EMAIL
     
     # Church staff that will receive prayer request notifications
-    recipients = [TICO_GMAIL_EMAIL, SUPPORT_ELKTON_CHURCH_EMAIL]
+    recipients = [
+        PASTOR_REGGIE_EMAIL, 
+        SUE_COURLISS_EMAIL, 
+        TICO_GMAIL_EMAIL, 
+        DON_FAUPEL_EMAIL,
+        CASEY_TURNER_EMAIL, 
+        MIKE_VIERS_EMAIL,
+        MELISSA_HELMUTH_EMAIL,
+        KIM_KARL_EMAIL,
+        KELLEE_ROTH_EMAIL,
+        PATTY_EVANS_EMAIL,
+        JEN_VIERS_EMAIL
+    ]
     
     
     msg = MIMEText(message, 'html')
